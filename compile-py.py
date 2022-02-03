4f8bcf6f1174a8058efc43b2ee7ca48fce7efddb
bg117 - The OpenCode Team <76200573+bg117@users.noreply.github.com>
2022-02-03 14:36:04 +0800

    fix all problems present

--- compile-py.py
+++ compile-py.py
@@ -1,23 +1,34 @@
+#!/usr/bin/env python3
+
+from re import T
 import sys
 from zipfile import ZipFile
 import os
+import zipfile
 
 
 def list_to_str(lst: list) -> str:
     return ' '.join(lst)
 
-# concat multiple scripts (for *nix OSes only)
-
 
 if len(sys.argv) == 1:
-    print('Usage: python3 compile-py.py <scripts_dir> <output_file>')
+    print(
+        'Usage: python3 compile-py.py <scripts_dir> <output_file> [--no-ext]')
     sys.exit(0)
 
-print('Compile Python scripts into a UNIX executable')
+print('Compile multiple Python scripts into a single .py file')
 
 # path is in argv[1]
 scripts_dir = sys.argv[1]
 out = sys.argv[2]
+
+# if out doesn't end with .py, add it
+if not out.endswith('.py'):
+    out += '.py'
+
+# check if "--no-ext" is in argv
+if '--no-ext' in sys.argv:
+    out = out[:-3]
 
 # check if scripts_dir exists
 if not os.path.isdir(scripts_dir):
@@ -39,19 +50,9 @@ if not os.path.isfile(os.path.join(scripts_dir, '__main__.py')):
     print('__main__.py does not exist in {}'.format(scripts_dir))
     sys.exit(1)
 
-# convert the files to a .zip file using ZipFile
-with ZipFile(out, 'w') as zip_file:
+# convert files into .zip file with relative paths
+with ZipFile(out, 'w') as zip:
     for file in files:
-        print('Adding {} to {}'.format(file, out))
-        zip_file.write(os.path.join(file))
+        zip.write(file, os.path.relpath(file, scripts_dir))
 
-# prepend #!/usr/bin/env python3 to the zip file
-with open(out, 'r+b') as zip_file:
-    zip_file.seek(0)
-    zip_file.write(b'#!/usr/bin/env python3\n')
-    zip_file.seek(0)
-
-# make the zip file executable
-os.chmod(out, 0o755)
-
-print('Done! Output file: ' + out)
+print('Done! You can run the output file with:\n\tpython3 {}'.format(out))
